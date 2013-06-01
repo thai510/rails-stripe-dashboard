096bd841331a53157b345d76346685f929a2ee10
Ian Alexander <ian@repairtechsolutions.com>
2013-06-01 11:39:46 -0700

    initial commit

---
+++ rails-stripe-dashboard/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsStripeDashboard::Application.config.secret_token = '437a013ffbd7279e46eabe7108f97cec9e1d603aeb6c84c684c11425229a933d022498bc4f5b22f5aece614ba25598955ecd3c6405930b532c0830bf2a19e41e'
